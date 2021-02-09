b66d2be696cc1e1a66e3d1c7d553309fd85dee8c
kgurudut <kgurudut@in.ibm.com>
2021-02-09 20:00:54 +0530

    tf archive_file changes

--- main.tf
+++ main.tf
@@ -7,29 +7,10 @@ data "ibm_app_domain_shared" "domain" {
   name = "mybluemix.net"
 }
 
-# data "archive_file" "app" {
-#   type        = "zip"
-#   source_dir = "src"
-#   output_path = "app.zip"
-# }
-
-resource "null_resource" "prepare_app_zip" {
-  triggers = {
-    app_version = var.app_version
-    git_repo    = var.git_repo
-  }
-
-  provisioner "local-exec" {
-    command = <<EOF
-        mkdir -p ${var.dir_to_clone}
-        cd ${var.dir_to_clone}
-        git clone ${var.git_repo}
-        cd ${var.source_dir}
-        zip -r ${var.app_zip} *
-        
-EOF
-
-  }
+data "archive_file" "app" {
+  type        = "zip"
+  source_dir = "src"
+  output_path = "app.zip"
 }
 
 resource "ibm_app_route" "approute-demo-001" {
@@ -39,12 +20,9 @@ resource "ibm_app_route" "approute-demo-001" {
 }
 
 resource "ibm_app" "cf-demo-001" {
-    depends_on = [
-    null_resource.prepare_app_zip,
-  ]
   name                 = var.app_name
   space_guid           = data.ibm_space.spacedata.id
-  app_path             = var.app_zip
+  app_path             = "app.zip"
   buildpack            = "python_buildpack"
   route_guid           = [ibm_app_route.approute-demo-001.id]
   app_version          = "1"
